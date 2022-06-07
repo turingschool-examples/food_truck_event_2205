596613e69ab6f89a4af05ac36476d05053f02e29
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 09:04:07 -0600

    Create tests for initialization

---
+++ spec/item_spec.rb
@@ -0,0 +1,33 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new(
+      {
+        name: 'Peach Pie (Slice)',
+        price: "$3.75"
+        }
+      )
+    @item2 = Item.new(
+      {
+        name: 'Apple Pie (Slice)',
+        price: '$2.50'
+        }
+      )
+  end
+
+  describe '#initialize' do
+    it 'is an item' do
+      expect(@item1).to be_a Item
+    end
+
+    it 'has a name' do
+      expect(@item1.name).to eq 'Peach Pie (Slice)'
+    end
+
+    it 'has a price' do
+      expect(@item1.price).to eq 3.75
+    end
+  end
+
+end
