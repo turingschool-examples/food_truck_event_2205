6a6dd06139070b2a3ba21c6b56256a9473521497
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 11:17:27 -0400

    Test: return 0 when item not stocked

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,10 +1,13 @@
 require './lib/food_truck'
+require './lib/item'
 require 'rspec'
 
 RSpec.describe FoodTruck do
 
   before (:each) do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   it 'exists' do
@@ -17,5 +20,9 @@ RSpec.describe FoodTruck do
 
   it 'initializes with empty inventory hash' do
     expect(@food_truck.inventory).to eq({})
+  end
+
+  it 'returns 0 when item not in stock' do
+    expect(@food_truck.check_stock(@item1)).to eq(0)
   end
 end
