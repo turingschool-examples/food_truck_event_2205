cc86df469bc538e67736181bcbcd2ba869e71b4e
Mary Turpin <maryvirginiaturpin@gmail.com>
2022-06-07 09:50:12 -0600

    iteration 1

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,25 @@
 class FoodTruck
 
-  attr_reader :name
+  attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock
+    @inventory.values.sum
+  end
+
+  def stock(key, value)
+    if @inventory == {}
+      @inventory = {
+      key => value
+      }
+    elsif @inventory.key?(key)
+      @inventory[key] += value
+    elsif @inventory.empty? == false
+      @inventory[key] = value
+    end
   end
 end
