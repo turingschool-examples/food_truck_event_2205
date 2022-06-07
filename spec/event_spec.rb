0ab9d59f95b96ed6a902589dba5e2f3b5789524e
Eli Sachs <easachs13@gmail.com>
2022-06-07 11:46:42 -0600

    Write it4 Event tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -90,4 +90,42 @@ RSpec.describe Event do
     end
   end
 
+  describe "#Iteration 4" do
+
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+
+    it "has a date" do
+      # don't know how to use stubs
+      expect(event.date).to be_a(Date)
+    end
+
+    xit "can sell" do
+      # out of time
+      expect(event.sell(item1, 200)).to be(false)
+      expect(event.sell(item5, 1)).to be(false)
+      expect(event.sell(item4, 5)).to be(true)
+      expect(food_truck2.check_stock(item4)).to eq(45)
+      expect(event.sell(item1, 40)).to be(true)
+      expect(food_truck1.check_stock(item1)).to eq(0)
+      expect(food_truck3.check_stock(item1)).to eq(60)
+    end
+  end
+
 end
