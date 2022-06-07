7cdb65637ca57386c2f2356953a298819a4d3508
philmarcu <philmark18@gmail.com>
2022-06-07 11:14:25 -0600

    Complete thru iteration 3, Finish Date method

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,5 @@
 class Event
+  require 'time'
   attr_reader :name, :food_trucks
   def initialize(name)
     @name = name
@@ -15,5 +16,52 @@ class Event
 
   def food_trucks_that_sell(item)
     @food_trucks.find_all {|truck| truck.inventory.keys.include?(item)}
+  end
+
+  def sorted_item_list
+    unsorted = @food_trucks.map do |truck|
+      truck.inventory.map do |item|
+        item[0]
+      end
+    end.flatten.uniq
+    sorted = unsorted.sort_by do |item|
+      item.name
+    end
+  end
+
+  def overstocked_items
+    pop_items = []
+    @food_trucks.select do |truck|
+      truck.inventory.find_all do |item, quantity|
+        if quantity >= 50 && food_trucks_that_sell(item).count > 1
+          pop_items << item
+        end
+      end
+    end
+    pop_items
+  end
+
+  def items_array
+    @food_trucks.flat_map {|truck| truck.inventory.keys}.uniq
+  end
+
+  def total_inventory
+    hash = {}
+    items_array.each do |item|
+      sum = food_trucks.sum do |truck|
+        if truck.inventory.keys.include?(item)
+          truck.inventory[item]
+        else
+          0
+        end
+      end
+      hash[item] = {quantity: sum, food_trucks: food_trucks_that_sell(item)}
+    end
+    hash
+  end
+
+  def date
+    today = Date.today
+    return today.to_s
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,6 +1,7 @@
 require './lib/item'
 require './lib/food_truck'
 require './lib/event'
+require 'date'
 
 RSpec.describe Event do
   let(:event) {Event.new("South Pearl Street Farmers Market")}
@@ -20,6 +21,7 @@ RSpec.describe Event do
     food_truck2.stock(item4, 50)
     food_truck2.stock(item3, 25)
     food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
   end
 
   it 'exists' do
@@ -46,5 +48,54 @@ RSpec.describe Event do
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
     expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
+  end
+
+  it 'can get a sorted_item_list' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.sorted_item_list).to eq([item2, item4, item1, item3])
+  end
+
+  it 'can get overstocked_items' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.overstocked_items).to eq([item1])
+  end
+
+  it 'has an array of all item objects' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.items_array.size).to eq(4)
+    expect(event.items_array.first.name).to eq("Peach Pie (Slice)")
+  end
+
+  it 'can get total_inventory' do
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    expected = {
+      item1 => {quantity: 100,
+                food_trucks: [food_truck1, food_truck3]
+              },
+      item2 => {quantity: 7,
+                food_trucks: [food_truck1]
+              },
+      item4 => {quantity: 50,
+                food_trucks: [food_truck2]
+      },
+      item3 => {quantity: 35,
+                food_trucks: [food_truck2, food_truck3]
+          }
+        }
+    expect(event.total_inventory).to eq(expected)
+  end
+
+  it 'can get the event date' do
+    allow(Date).to receive(:today).and_return Date.new(1993,5,12)
+    expect(event.date).to eq("1993-05-12")
   end
 end
