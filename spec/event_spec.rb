efdbf310771d60af6d90ba0a604a727443ffdead
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 12:09:54 -0400

    create event spec and two tests

---
+++ spec/event_spec.rb
@@ -0,0 +1,25 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  before(:each) do
+    @event = Event.new("South Pearl Street Farmers Market")
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    @food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+  end
+
+  it 'exists' do
+    expect(@event).to be_an(Event)
+  end
+
+  it 'has a name' do
+    expect(@event.name).to eql("South Pearl Street Farmers Market")
+  end
+
+end
