a4db4999c49a1921ef162dca8c67105309911c20
Nikky Rojas <rojas7313@gmail.com>
2022-06-07 09:43:50 -0600

    start food truck methods

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,15 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
     @inventory = {}
   end
+  def check_stock
+
+  end
+
+  def stock
+
+  end
+
 end
