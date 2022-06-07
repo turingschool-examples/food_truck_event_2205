cc442d6138c768d897589dd382942a9b1b87953a
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-07 12:46:02 -0400

    Add tests for iter-3

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -62,4 +62,36 @@ RSpec.describe Event do
     expect(@food_truck2.potential_revenue).to eq(345.00)
     expect(@food_truck3.potential_revenue).to eq(243.75)
   end
+
+  it 'can list overstocked items' do
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.overstocked_items).to eq([@item1])
+  end
+
+  it 'can sort item names' do
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
+  end
+
+  it 'can return total inventory' do
+    expect(@event.total_inventory).to eq({@item1 => {quantity: 100, food_trucks: [@food_truck1, @food_truck3]}, @item2 => {quantity: 7, food_trucks: [@food_truck1]}, @item_3 => {quantity: 35, food_trucks: [@food_truck2, @food_truck3]}, @item4 => {quantity: 50, food_trucks: [@food_truck2]}})
+  end
 end
