9f4204eef991b214aa9a20c20e2516989cc75fe6
Lucas Colwell <colwellc37@gmail.com>
2022-06-07 11:00:45 -0600

    Feat: add sorted items list method

--- lib/event.rb
+++ lib/event.rb
@@ -7,4 +7,42 @@ class Event
     @food_trucks = []
   end
 
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    names = @food_trucks.map {|truck| truck.name}
+  end
+
+  def food_trucks_that_sell(item)
+    trucks = []
+    @food_trucks.each do |truck| #could find_all be used for hash value?
+      if truck.check_stock(item) > 0
+        trucks << truck
+      end
+    end
+    return trucks
+  end
+
+  def overstocked_items #stuck, moving forward and will come back
+    overstock_list = []
+    food_trucks.each do |truck|
+      truck.inventory.keys.each do |item|
+        if truck.check_stock(item) >= 50
+          overstock_list << item
+        end
+      end
+      # require "pry"; binding.pry
+      overstock_list
+    end
+  end
+
+  def sorted_item_list
+    list = []
+    @food_trucks.each do |truck|
+      truck.inventory.map {|item| list << item[0].name}
+    end
+    return list.uniq
+  end
 end
