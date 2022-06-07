8b4d88574b238d1703596362b356e7adf17f5b5f
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-07 11:46:14 -0400

    Refactor

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,19 +2,19 @@ require './lib/item'
 
 RSpec.describe Item do
   before :each do
-    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   it 'exists' do
-    expect(item1).to be_an(Item)
+    expect(@item1).to be_an(Item)
   end
 
   it 'has a name' do
-    expect(item2.name).to eq("Apple Pie (Slice)")
+    expect(@item2.name).to eq("Apple Pie (Slice)")
   end
 
   it 'has a price' do
-    expect(item2.price).to eq(2.50)
+    expect(@item2.price).to eq(2.50)
   end
 end
