e7613e9970bb565c9a317e7158de04abd7de0b23
Nikky Rojas <rojas7313@gmail.com>
2022-06-07 11:27:07 -0600

    event methods

--- lib/event.rb
+++ lib/event.rb
@@ -1,11 +1,16 @@
 class Event
-  attr_reader :name
+  attr_reader :name, :food_truck
   def initialize(name)
     @name = name
+    @food_trucks = []
   end
 
-  def add_food_truck
+  def add_food_truck(food_truck)
+    food_truck << food_truck
+  end
 
+  def food_truck_names
+    food_truck.all.name
   end
 
 
