28b9e20236cb4fde534626fe00faecf1e2b8b0b3
Justin Ramirez <jusrez528@gmail.com>
2022-06-07 14:00:36 -0400

    Iteration 3 progress

--- lib/event.rb
+++ lib/event.rb
@@ -18,11 +18,57 @@ class Event
 	def food_trucks_that_sell(item)
 		food_trucks_that_sell = []
 		@food_trucks.each do |food_truck|
-			food_truck.inventory.each do |key, value|
-				food_trucks_that_sell << food_truck if key == item
+			food_truck.inventory.each do |item_instance, _quantity|
+				food_trucks_that_sell << food_truck if item_instance == item
 			end
 		end
 		return food_trucks_that_sell
+	end
+
+	def overstocked_items
+		more_than_one_truck = []
+		overstocked_items = []
+		items_by_food_truck.each do |item, food_trucks_with_item|
+			more_than_one_truck << item if food_trucks_with_item > 1
+		end
+		item_totals.each do |item, quantity|
+			overstocked_items << item if quantity > 50
+		end
+		return overstocked_items
+	end
+
+	def items_by_food_truck
+		items_by_truck = Hash.new(0)
+		@food_trucks.each do |food_truck|
+			food_truck.inventory.each do |item, _quantity|
+			items_by_truck[item] += 1
+			end
+		end
+		return items_by_truck
+	end
+
+	def item_totals
+		item_totals = Hash.new(0)
+		@food_trucks.each do |food_truck|
+			food_truck.inventory.each do |item, quantity|
+			item_totals[item] += quantity
+			end
+		end
+		return item_totals
+	end
+
+	def sorted_item_list
+		item_names = []
+		item_totals.each do |item, quantity|
+			item_names << item.name
+		end
+		return item_names.sort
+	end
+
+	def total_inventory
+		total_inventory = {}
+		item_totals.each do |item, quantity|
+			total_inventory[item] = {quantity: quantity, food_trucks:}
 	end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -109,6 +109,81 @@ RSpec.describe Event do
 			expect(food_truck3.potential_revenue).to eq(243.75)
 		end
 
+	end
+	describe 'Iteration 3' do
+		it 'can return a list of overstocked items' do
+			event = Event.new("South Pearl Street Farmers Market")
+			item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+			item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+			item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+			item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+			food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+			food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+			food_truck3 = FoodTruck.new("Palisade Peach Shack")
 
+			food_truck1.stock(item1, 35)
+			food_truck1.stock(item2, 7)
+			food_truck2.stock(item4, 50)
+			food_truck2.stock(item3, 25)
+			food_truck3.stock(item1, 65)
+			food_truck3.stock(item3, 10)
+
+			event.add_food_truck(food_truck1)
+			event.add_food_truck(food_truck2)
+			event.add_food_truck(food_truck3)
+
+			expect(event.overstocked_items.count).to eq(1)
+			expect(event.overstocked_items).to eq([item1])
+
+		end
+
+		it 'can return a sorted list of item names for an event' do
+			event = Event.new("South Pearl Street Farmers Market")
+			item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+			item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+			item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+			item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+			food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+			food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+			food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+			food_truck1.stock(item1, 35)
+			food_truck1.stock(item2, 7)
+			food_truck2.stock(item4, 50)
+			food_truck2.stock(item3, 25)
+			food_truck3.stock(item1, 65)
+			food_truck3.stock(item3, 10)
+
+			event.add_food_truck(food_truck1)
+			event.add_food_truck(food_truck2)
+			event.add_food_truck(food_truck3)
+			event.sorted_item_list
+			expect(event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
+
+		end
+
+		it 'can return a hash of total inventory from an Event' do
+			event = Event.new("South Pearl Street Farmers Market")
+			item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+			item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+			item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+			item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+			food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+			food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+			food_truck3 = FoodTruck.new("Palisade Peach Shack")
+
+			food_truck1.stock(item1, 35)
+			food_truck1.stock(item2, 7)
+			food_truck2.stock(item4, 50)
+			food_truck2.stock(item3, 25)
+			food_truck3.stock(item1, 65)
+			food_truck3.stock(item3, 10)
+
+			event.add_food_truck(food_truck1)
+			event.add_food_truck(food_truck2)
+			event.add_food_truck(food_truck3)
+
+			expect(event.total_inventory).to be_a(Hash)
+		end
 	end
 end
