efd66a0c32fd94be4d89b2b562530e032a2c80b2
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-07 10:05:17 -0500

    feat: Initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'is an Item' do
+    expect(@item1).to be_instance_of(Item)
+  end
+end
