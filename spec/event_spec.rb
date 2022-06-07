88fb08f4a5fdaec16c85c180009d2db92fdbd453
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 11:49:18 -0400

    Test: return trucks selling given item

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -52,4 +52,12 @@ RSpec.describe Event do
     expect(@event.food_truck_names).to eq(["Rocky Mountain Pies","Ba-Nom-a-Nom","Palisade Peach Shack"])
   end
 
+  it 'returns all trucks that sell given item' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1,@food_truck3])
+  end
+
 end
