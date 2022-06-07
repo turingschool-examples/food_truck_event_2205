7834ffd2237d286ae5743d9cc78f58e40f79450b
Alex M <Alexm717@hotmail.com>
2022-06-07 10:14:34 -0700

    minor changes in test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe(FoodTruck) do
     expect(@food_truck).to(be_a(FoodTruck))
   end
 
-  it("#name of pie ") do
+  it("#name of truck ") do
     expect(@food_truck.name).to(eq("Rocky Mountain Pies"))
   end
 
@@ -19,8 +19,16 @@ RSpec.describe(FoodTruck) do
     expect(@food_truck.inventory).to(eq({}))
   end
 
-  it("#can check the stock")
-  @food_truck = FoodTruck.new("Rocky Mountain Pies")
-  @item1 = Item.new({name: "Peach Pie (Slice)", price: "$3.75"})
-  expect(@food_truck.check_stock(@item1)).to(eq((0)))
+  it("#can check the stock") do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: "Peach Pie (Slice)", price: "$3.75"})
+    expect(@food_truck.check_stock(@item1)).to(eq((0)))
+  end
+
+  it("#shovel stock into invertory hash") do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: "Peach Pie (Slice)", price: "$3.75"})
+    @food_truck.stock(@item1, 30)
+    expect(@food_truck.inventory).to(eq(@item1, 30))
+  end
 end
