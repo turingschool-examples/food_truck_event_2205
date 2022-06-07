380931ac9c59c92017a4da5a7c637ad81f22d456
Jason Noble <github+jasonn@jasonnoble.org>
2022-06-07 11:38:35 -0600

    Add Event#food_trucks_that_sell

--- lib/event.rb
+++ lib/event.rb
@@ -10,4 +10,8 @@ class Event
   def food_truck_names
     @food_trucks.map(&:name)
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select { |food_truck| food_truck.check_stock(item).positive? }
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 require_relative '../lib/food_truck'
 require_relative '../lib/event'
+require_relative '../lib/item'
 
 RSpec.describe Event do
   let(:event) { described_class.new }
   let(:food_truck) { FoodTruck.new("Rocky Mountain Pies") }
+  let(:pie_truck) do
+    truck = FoodTruck.new("A La Mode")
+    truck.stock(peach_pie, 10)
+    truck.stock(apple_pie, 20)
+    truck
+  end
+  let(:peach_pie) { Item.new({ name: 'Peach Pie', price: "$3.75" }) }
+  let(:apple_pie) { Item.new({ name: 'Apple Pie', price: "$2.75" }) }
 
   describe "#add_food_truck(food_truck)" do
     it "adds the truck to the list" do
@@ -26,6 +35,23 @@ RSpec.describe Event do
 
       it "returns no names" do
         expect(event.food_truck_names).to include(food_truck.name)
+      end
+    end
+  end
+
+  describe "#food_trucks_that_sell(item)" do
+    context "when there are no food trucks that sell it" do
+      it "lists no food trucks" do
+        event.add_food_truck(food_truck)
+        expect(event.food_trucks_that_sell(apple_pie)).to eq([])
+      end
+    end
+
+    context "when there are food trucks that sell it" do
+      it "lists no food trucks" do
+        event.add_food_truck(food_truck)
+        event.add_food_truck(pie_truck)
+        expect(event.food_trucks_that_sell(apple_pie)).to eq([pie_truck])
       end
     end
   end
