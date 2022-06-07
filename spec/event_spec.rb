585e722d8f0d3d81dbf5629e6b78593d0f224e07
Thiago Silveira <thiago1992@yahoo.com>
2022-06-07 09:48:12 -0600

    Test: Event exists

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,5 +3,8 @@ require './lib/food_truck'
 require './lib/event'
 
 RSpec.describe Event do
-
+  it 'exists' do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event).to be_a(Event)
+  end
 end
