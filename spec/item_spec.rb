d495a91b3dca5e1ea0b41cbcd05379f6218bbd9e
Drew MacNicholas <dmacnicholas@gmail.com>
2022-06-07 10:09:46 -0500

    Test/Add in item exists test

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+    expect(@item2).to be_a(Item)
+  end
+end
+  
