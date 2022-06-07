53e7d0e76e2939ac5d797a8183c22ea36b0bbbe6
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 12:47:12 -0400

    Feat: sorted_item_list

--- lib/event.rb
+++ lib/event.rb
@@ -20,15 +20,32 @@ require 'pry'
     @food_trucks.select {|truck| truck.inventory.key?(item)}
   end
 
+  def sorted_item_list
+    all_item_names = []
+    @food_trucks.each do |truck|
+      truck.inventory.each do |inventory_item|
+        all_item_names << inventory_item[0].name
+      end
+    end
+    all_item_names.uniq.sort
+  end
+
   def sold_by_more_than_one_truck?(item)
     all_inventory = []
     @food_trucks.each do |truck|
       truck.inventory.each do |inventory_item|
-        all_inventory << inventory_item[0].name
+        all_inventory << inventory_item[0]
       end
     end
-    all_inventory.count(item.name) > 1
+    all_inventory.count(item) > 1
   end
+
+  # def total_inventory_by_item(item)
+  #   all_inventory =
+  #   @food_trucks.each do |truck|
+  #
+  #   end
+  # end
 
   # def overstocked_items
   #   items_and_inventory = {}
