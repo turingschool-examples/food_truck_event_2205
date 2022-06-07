7b6f735a14f503226b7406dbcd89ac4662a0c574
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-07 10:59:32 -0600

    feat: fixed typo and completed item methods and tests for iteration 1

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe Item do
 
   it "returns the item name" do
     expect(@item1.name).to eq("Peach Pie (Slice)")
-    expect(@iten2.name).to eq("Apple Pie (Slice)")
+    expect(@item2.name).to eq("Apple Pie (Slice)")
   end
 
   it "returns the item price" do
     expect(@item1.price).to eq("$3.75")
-    expect(@iten2.price).to eq("$2.50")
+    expect(@item2.price).to eq("$2.50")
   end
 end
