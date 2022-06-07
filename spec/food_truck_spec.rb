4282f5eea95beab2064b8e1a42d956e76825f4cd
Lucas Colwell <colwellc37@gmail.com>
2022-06-07 09:18:35 -0600

    Test: create test for food truck class (it1)

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,12 +1,13 @@
 require 'rspec'
-require "./lib/item"
+require ''./lib/item'
 require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   describe 'Iteration 1' do
     before :each do
-      @
-      @
+      @food_truck = FoodTruck.new("Rocky Mountain Pies")
+      @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     end
 
     it 'exists' do
@@ -14,9 +15,29 @@ RSpec.describe FoodTruck do
     end
 
     it 'has attributes' do
-      expect(@food_truck.attribute).to eq()
+      expect(@food_truck.name).to eq("Rocky Mountain Pies")
+      expect(@food_truck.inventory).to eq({})
     end
 
-  end
+    it 'can check stock' do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
+    end
 
+    it 'can add items to stock' do
+      expect(@food_truck.check_stock(@item1)).to eq(0)
+
+      @food_truck.stock(@item1, 30)
+
+      expect(@food_truck.inventory).to eq({@item1 => 30})
+      expect(@food_truck.check_stock(@item1)).to eq(30)
+
+      @food_truck.stock(@item1, 25)
+
+      expect(@food_truck.check_stock(@item1)).to eq(55)
+
+      @food_truck.stock(@item2, 12)
+
+      expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+  end
 end
