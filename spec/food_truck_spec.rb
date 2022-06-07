0c2f26fd795cd9aa06753d8aebe9b494d0afe0af
Thiago Silveira <thiago1992@yahoo.com>
2022-06-07 09:16:38 -0600

    Fix: Food truck spec has attributes

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe FoodTruck do
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     expect(food_truck).to be_a(FoodTruck)
   end
+
+  it 'has attributes' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
+
+
 end
