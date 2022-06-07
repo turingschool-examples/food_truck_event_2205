2dc05d95c132fa9ba1655f5bed5c836fb7ea5baa
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 11:22:12 -0400

    Test: can add items to stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -25,4 +25,10 @@ RSpec.describe FoodTruck do
   it 'returns 0 when item not in stock' do
     expect(@food_truck.check_stock(@item1)).to eq(0)
   end
+
+  it 'can add items to stock' do
+    @food_truck.stock(@item1,30)
+
+    expect(@food_truck.inventory).to eq({@item1 => 30})
+  end
 end
