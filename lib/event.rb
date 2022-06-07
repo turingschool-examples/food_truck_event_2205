3f05401e2ebc355445f7141e4ae63fa717fc8b16
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 10:14:49 -0600

    Create sorted_item_list to pass tests

--- lib/event.rb
+++ lib/event.rb
@@ -21,7 +21,18 @@ class Event
     end
   end
 
-  def overstocked_items
-    overstocked = []
+  # def overstocked_items
+  #   overstocked = []
+  #
+  # end
+
+  def sorted_item_list
+    items = []
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, _|
+        items << item.name
+      end
+    end
+    items.sort.uniq!
   end
 end
