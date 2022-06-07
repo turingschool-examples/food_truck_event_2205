14b322e08cb29211842925f56be6a14b3a3a23e4
Eli Sachs <easachs13@gmail.com>
2022-06-07 09:21:50 -0600

    Write FoodTruck spec

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,38 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+
+  food_truck = FoodTruck.new("Rocky Mountain Pies")
+  item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+  item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+  it "exists" do
+    expect(food_truck).to be_a(FoodTruck)
+  end
+
+  it "has a name" do
+    expect(food_truck.name).to eq("Rocky Mountain Pies")
+  end
+
+  it "begins with empty inventory" do
+    expect(food_truck.inventory).to eq({})
+  end
+
+  it "can check stock" do
+    expect(food_truck.check_stock(item1)).to eq(0)
+  end
+
+  it "can stock an item" do
+    food_truck.stock(item1, 30)
+    expect(food_truck.inventory).to include(item1)
+    expect(food_truck.inventory[item1]).to eq(30)
+    expect(food_truck.check_stock(item1)).to eq(30)
+    food_truck.stock(item1, 25)
+    expect(food_truck.check_stock(item1)).to eq(55)
+    food_truck.stock(item2, 12)
+    expect(food_truck.inventory).to include(item2)
+    expect(food_truck.check_stock(item2)).to eq(12)
+  end
+
+end
