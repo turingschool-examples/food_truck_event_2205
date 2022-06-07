80200fe8a25a2316c7413bbab82facf20f0c06ff
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-07 14:00:37 -0400

    Update methods

--- lib/event.rb
+++ lib/event.rb
@@ -16,4 +16,33 @@ class Event
   def food_trucks_that_sell(item)
     @food_trucks.find_all {|truck| truck.check_stock(item) > 0}
   end
+
+  def all_items
+    @food_trucks.flat_map do |truck|
+      truck.inventory.map do |item, qty|
+        item
+      end
+    end
+  end
+
+  def unique_items
+    items.uniq
+  end
+
+  def sorted_item_list
+    unique_items.name.sort
+  end
+
+  def total_inventory
+    inventory = {}
+    unique_items.each do |item|
+      food_trucks_that_sell(item)
+
+  end
+
+  def overstocked_items
+
+    require "pry"; binding.pry
+
+  end
 end
