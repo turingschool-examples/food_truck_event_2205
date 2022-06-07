43bd74b0ccbc600e71d2804852b128b3864e9330
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 12:11:32 -0400

    Test: overstocked_items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -19,9 +19,12 @@ RSpec.describe Event do
 
     @food_truck1.stock(@item1, 35)
     @food_truck1.stock(@item2, 7)
+
     @food_truck2.stock(@item4, 50)
     @food_truck2.stock(@item3, 25)
+
     @food_truck3.stock(@item1, 65)
+    @food_truck3.stock(@item3, 10)
   end
 
   it 'exists' do
@@ -60,6 +63,14 @@ RSpec.describe Event do
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1,@food_truck3])
 
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
+  end
+
+  it 'returns overstocked items' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+
+    expect(@event.overstocked_items).to eq([@item1])
   end
 
 end
