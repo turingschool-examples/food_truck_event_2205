c4ad361a0cae0b61f882eec6aa113d2370c0602a
Nikky Rojas <rojas7313@gmail.com>
2022-06-07 11:39:49 -0600

    event test fix

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,7 +5,12 @@ require 'rspec'
 require 'pry'
 
 RSpec.describe Event do
-  before(:each) do
+  it "event exist" do
+    event = Event.new("South Pearl Street Farmers Market")
+    expect(event).to be_a(Event)
+  end
+
+  xit "can add a food truck to event" do
     event = Event.new("South Pearl Street Farmers Market")
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
@@ -20,18 +25,27 @@ RSpec.describe Event do
     food_truck2.stock(item3, 25)
     food_truck3.stock(item1, 65)
     food_truck3.stock(item3, 10)
-  end
-
-  it "event exist" do
-    expect(event).to be_a Event
-  end
-
-  it "can add a food truck to event" do
     event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
     expect(event.add_food_truck(food_truck1)).to eq()
   end
 
-  it "can list food truck names" do
+  xit "can list food truck names" do
+    event = Event.new("South Pearl Street Farmers Market")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
     event.add_food_truck(food_truck1)
     event.add_food_truck(food_truck2)
     event.add_food_truck(food_truck3)
