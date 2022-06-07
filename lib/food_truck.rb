0bc0825a2789e74d485ba9e4dd9a314e616874a1
Alex M <Alexm717@hotmail.com>
2022-06-07 10:15:07 -0700

    methods to food_truck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -3,6 +3,14 @@ class FoodTruck
 
   def initialize(name)
     @name = name
+
+    #maybe??
+    # i know this is wrong but i dont know how to fix it
     @inventory = {}
+    @stock = 0
+  end
+
+  def check_stock(item)
+    @stock
   end
 end
