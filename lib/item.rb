9cf79e9b10dd4c840ec51bb2a79d15afb7e86c6b
Blake Saylor <blakesaylor@gmail.com>
2022-06-07 11:26:44 -0600

    Add note to fix price output

--- lib/item.rb
+++ lib/item.rb
@@ -1,6 +1,8 @@
 class Item
   attr_reader :name, :price
 
+
+  # NEED TO FIX PRICE TO OUTPUT 2 DECIMAL PLACES ???
   def initialize(attributes)
     @name = attributes[:name]
     @price = attributes[:price].gsub('$', '').to_f
