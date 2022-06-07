51a0dbd588cb1e87137c53277643b9dedc610b25
Thiago Silveira <thiago1992@yahoo.com>
2022-06-07 09:13:21 -0600

    Test: Food truck exists

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -2,5 +2,8 @@ require './lib/item'
 require './lib/food_truck'
 
 RSpec.describe FoodTruck do
-
+  it 'exists' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    expect(food_truck).to be_a(FoodTruck)
+  end
 end
