aa2626527b2ec19ff1ef6813e78d129077ae56e8
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-07 11:44:59 -0400

    Create class and add methods for iter-1

---
+++ lib/food_truck.rb
@@ -0,0 +1,15 @@
+class FoodTruck
+  attr_reader :name, :inventory
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def stock(item, qty)
+    @inventory[item] += qty
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+end
