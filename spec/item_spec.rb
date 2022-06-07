0e71c921dae78c1f410a703eeafe6f4e6e7a550d
Bryce Simonds <bsimonds14@gmail.com>
2022-06-07 11:03:52 -0600

    Debug: changed expect in attributes test from string to float

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Item do
   it 'has attributes' do
     expect(@item1.name).to eq("Peach Pie (Slice)")
     expect(@item2.name).to eq( "Apple Pie (Slice)")
-    expect(@item1.price).to eq("$3.75")
-    expect(@item2.price).to eq("$2.50")
+    expect(@item1.price).to eq(3.75)
+    expect(@item2.price).to eq(2.50)
   end
 end
