57ae82664adeec0cf3bd7a7f1d8ee8b40114a9d1
Bryce Simonds <bsimonds14@gmail.com>
2022-06-07 09:15:02 -0600

    Test: it exists test

---
+++ spec/foodtruck_spec.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  before :each do
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  it 'exists' do
+    expect(@food_truck).to be_a(FoodTruck)
+  end
+end
