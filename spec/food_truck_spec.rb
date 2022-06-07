0130ed41473873f27b9defabc5b99a561e793c33
Lucas Colwell <colwellc37@gmail.com>
2022-06-07 09:13:38 -0600

    Initial commit + structure

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require "./lib/item"
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  describe 'Iteration 1' do
+    before :each do
+      @
+      @
+    end
+
+    it 'exists' do
+      expect(@food_truck).to be_a(FoodTruck)
+    end
+
+    it 'has attributes' do
+      expect(@food_truck.attribute).to eq()
+    end
+
+  end
+
+end
