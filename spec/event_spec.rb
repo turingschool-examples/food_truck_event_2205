763b47e33b45bd1338af5fe680ae49ecc9caf843
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 11:35:12 -0400

    Test: event exists

---
+++ spec/event_spec.rb
@@ -0,0 +1,16 @@
+require './lib/food_truck'
+require './lib/item'
+require './lib/event'
+require 'rspec'
+
+RSpec.describe Event do
+
+  before (:each) do
+    @event = Event.new("South Pearl Street Farmers Market")
+  end
+
+  it 'exists' do
+    expect(@event).to be_instance_of(Event)
+  end
+
+end
