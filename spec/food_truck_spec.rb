db4e07a3a5fdf5238b8b076371c85576c1537214
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-07 10:11:31 -0500

    feat: add name attr

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe FoodTruck do
   it 'is a FoodTruck' do
     expect(@food_truck).to be_instance_of(FoodTruck)
   end
+
+  it 'has a name' do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+  end
 end
