1b0e6a3ebcbba8155998269e1cb90c68a577a9dc
Brenna Stuart <brennacodes@gmail.com>
2022-06-07 09:56:06 -0600

    Test: Event has food trucks - pass

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,16 @@
 require 'helper'
 
 class Event
-  attr_reader :name
+  attr_reader :name,
+              :food_trucks
+
 
   def initialize(name)
     @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck)
+
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,9 +1,13 @@
 require './lib/helper'
 
 RSpec.describe Event do
-  let!(:food_truck) {FoodTruck.new("Rocky Mountain Pies")}
+  let!(:food_truck1) {FoodTruck.new("Rocky Mountain Pies")}
+  let!(:food_truck2) {FoodTruck.new("Ba-Nom-a-Nom")}
+  let!(:food_truck3) {FoodTruck.new("Palisade Peach Shack")}
   let!(:item1) {Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})}
   let!(:item2) {Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})}
+  let!(:item3) {Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})}
+  let!(:item3) {Item.new({name: "Banana Nice Cream", price: "$4.25"})}
   let!(:event) {Event.new("South Pearl Street Farmers Market")}
 
   it "exists" do
@@ -12,5 +16,10 @@ RSpec.describe Event do
 
   it "has a name" do
     expect(event.name).to eq("South Pearl Street Farmers Market")
+  end
+
+  it "has food trucks" do
+    expect(event.food_trucks).to eq([])
+
   end
 end
