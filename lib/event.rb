9f8c9ef05eb83e54e11515ed949fd85a1a7cc779
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 13:15:25 -0400

    update event spec and event class to account for potential revenue by food truck

--- lib/event.rb
+++ lib/event.rb
@@ -2,10 +2,28 @@ require_relative 'item'
 require_relative 'food_truck'
 
 class Event
-  attr_accessor :name
+  attr_accessor :name, :food_trucks
 
   def initialize(name)
     @name = name
+    @food_trucks = []
   end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
+
+  def food_truck_names
+    all_food_truck_names = []
+    @food_trucks.each {|food_truck| all_food_truck_names << food_truck.name}
+    all_food_truck_names
+  end
+
+  def food_trucks_that_sell(item)
+    trucks = []
+    @food_trucks.each {|food_truck| trucks << food_truck if food_truck.inventory[item]}
+    trucks
+  end
+
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -22,4 +22,75 @@ RSpec.describe Event do
     expect(@event.name).to eql("South Pearl Street Farmers Market")
   end
 
+  it 'has no food trucks by default' do
+    expect(@event.food_trucks).to eql([])
+  end
+
+  it 'adds food trucks to the event' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.food_trucks).to eql([@food_truck1, @food_truck2, @food_truck3])
+    expect(@event.food_trucks.count).to eql(3)
+  end
+
+  it 'returns the names of the food trucks' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.food_truck_names).to eql(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+  it 'returns the names of the food trucks' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.food_truck_names).to eql(["Rocky Mountain Pies", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+  it 'finds food trucks that sell an item' do
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    require "pry"; binding.pry
+    expect(@event.food_trucks_that_sell(@item1).count).to eql(2)
+    expect(@event.food_trucks_that_sell(@item4).count).to eql(1)
+    expect(@event.food_trucks_that_sell(@item1)).to eql([@food_truck1, @food_truck3])
+    expect(@event.food_trucks_that_sell(@item4)).to eql([@food_truck2])
+  end
+
+  it 'finds food trucks that sell an item' do
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@event.food_trucks_that_sell(@item1).count).to eql(2)
+    expect(@event.food_trucks_that_sell(@item4).count).to eql(1)
+    expect(@event.food_trucks_that_sell(@item1)).to eql([@food_truck1, @food_truck3])
+    expect(@event.food_trucks_that_sell(@item4)).to eql([@food_truck2])
+  end
+
+  it 'finds potential revenue by food truck' do
+    @food_truck1.stock(@item1, 35)
+    @food_truck1.stock(@item2, 7)
+    @food_truck2.stock(@item4, 50)
+    @food_truck2.stock(@item3, 25)
+    @food_truck3.stock(@item1, 65)
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@food_truck1.potential_revenue).to eql(148.75)
+    expect(@food_truck2.potential_revenue).to eql(345.00)
+    expect(@food_truck3.potential_revenue).to eql(243.75)
+  end
+
 end
