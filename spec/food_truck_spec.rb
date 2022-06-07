bd08638d8f658d04ee31cc6224f676f4e862ebd0
Jason Noble <github+jasonn@jasonnoble.org>
2022-06-07 11:13:33 -0600

    Add more test cases for inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -5,6 +5,7 @@ require_relative '../lib/item'
 RSpec.describe FoodTruck do
   let(:food_truck) { described_class.new('Rocky Mountain Pies') }
   let(:peach_pie) { Item.new({ name: 'Peach Pie', price: "$3.75" }) }
+  let(:apple_pie) { Item.new({ name: 'Apple Pie', price: "$2.75" }) }
 
   it 'has a name' do
     expect(food_truck.name).to eq('Rocky Mountain Pies')
@@ -13,6 +14,21 @@ RSpec.describe FoodTruck do
   describe "#inventory" do
     it "is empty by default" do
       expect(food_truck.inventory).to eq({})
+    end
+
+    context "with items in inventory" do
+      before do
+        food_truck.stock(peach_pie, 30)
+        food_truck.stock(apple_pie, 50)
+      end
+
+      it 'includes peach pie' do
+        expect(food_truck.inventory).to have_key(peach_pie)
+      end
+
+      it 'includes apple pie' do
+        expect(food_truck.inventory).to have_key(apple_pie)
+      end
     end
   end
 
