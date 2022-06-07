e1d4302ddc69b583b6aac672facc51b570fc32cb
Lucas Colwell <colwellc37@gmail.com>
2022-06-07 09:26:23 -0600

    Test: refactor robust test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Item do
     end
 
     it 'has attributes' do
-      expect(@item1.name).to eq("Apple Pie (Slice)")
-      expect(@item1.price).to eq(2.50)
+      expect(@item1.name).to eq("Peach Pie (Slice)")
+      expect(@item1.price).to eq(3.75)
       expect(@item2.name).to eq("Apple Pie (Slice)")
       expect(@item2.price).to eq(2.50)
     end
