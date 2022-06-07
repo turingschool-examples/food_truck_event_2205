5b5a2672173289fc8598d605f270e374991d1954
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-07 12:10:28 -0400

    fix error in food truck spec

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,7 +3,7 @@ require './lib/food_truck'
 
 RSpec.describe FoodTruck do
   before(:each) do
-    @food_truck = food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
