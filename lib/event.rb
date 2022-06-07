082a4f1c31172e5d3fbcce5784fb12dcb5054b95
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-07 12:33:28 -0400

    Feat: sold_by_more_than_one_truck?

--- lib/event.rb
+++ lib/event.rb
@@ -1,4 +1,5 @@
 class Event
+require 'pry'
 
   attr_reader :name, :food_trucks
 
@@ -18,5 +19,24 @@ class Event
   def food_trucks_that_sell(item)
     @food_trucks.select {|truck| truck.inventory.key?(item)}
   end
+
+  def sold_by_more_than_one_truck?(item)
+    all_inventory = []
+    @food_trucks.each do |truck|
+      truck.inventory.each do |inventory_item|
+        all_inventory << inventory_item[0].name
+      end
+    end
+    all_inventory.count(item.name) > 1
+  end
+
+  # def overstocked_items
+  #   items_and_inventory = {}
+  #   @food_trucks.each do |truck|
+  #     truck.inventory.each do |item|
+  #       all_items[item.name]
+  #     end
+  #   end
+  # end
 
 end
