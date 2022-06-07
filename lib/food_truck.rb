4cacbe35570c0544653284d25a2e766adf19c5ea
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-07 11:28:06 -0600

    feat: completed iteration 2 with food_truck class

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,2 +1,24 @@
 class FoodTruck
+
+  attr_reader :name, :inventory, :stock_item, :quantity
+
+  def initialize(truck_name)
+    @name = truck_name
+    @inventory = []
+  end
+
+  def stock(stock_item, stock_quantity)
+    @inventory << {:stock_item => stock_item, :quantity => stock_quantity}
+  end
+
+  def check_stock(target_item)
+    current_stock = 0
+    @inventory.each do |item|
+      if item[:stock_item] == target_item
+        current_stock += item[:quantity]
+      end
+    end
+    current_stock
+  end
+
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -3,7 +3,7 @@ require './lib/item'
 
 RSpec.describe FoodTruck do
   before :each do
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: "$2.50"})
   end
@@ -17,15 +17,17 @@ RSpec.describe FoodTruck do
   end
 
   it "returns empty hash of inventory" do
-    expect(@food_truck.inventory).to eq({})
+    expect(@food_truck.inventory).to eq([])
   end
 
   it "can check the num of stocked items of given item search" do
-    expect(@food_truck.check_stock(item1)).to eq(0)
+    expect(@food_truck.check_stock(@item1)).to eq(0)
   end
 
   it "can stock given items and shows up in inventory" do
     @food_truck.stock(@item1, 30)
+
+
     expect(@food_truck.inventory.count).to eq(1)
     expect(@food_truck.check_stock(@item1)).to eq(30)
   end
