5c9ef064370ec39f6f2679598650d6ee64ded8ae
Thiago Silveira <thiago1992@yahoo.com>
2022-06-07 11:04:37 -0600

    Test: Event can return a list of names of all items a foodtruck has in stock

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Event do
     expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
   end
 
-  it 'can return a list of overstocked items' do
+  xit 'can return a list of overstocked items' do
     event = Event.new("South Pearl Street Farmers Market")
 
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
@@ -94,4 +94,33 @@ RSpec.describe Event do
 
     expect(event.overstocked_items).to eq(item1) # An item is overstocked if it is sold by more than 1 food truck AND the total quantity is greater than 50.
   end
+
+  it 'can return a list of names of all items a foodtruck has in stock' do
+    event = Event.new("South Pearl Street Farmers Market")
+
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    food_truck3.stock(item3, 10)
+
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+    expect(event.sorted_item_list).to eq(["Apple Pie (Slice)", "Banana Nice Cream", "Peach Pie (Slice)", "Peach-Raspberry Nice Cream"])
+    #sort alphabetically, no duplicates
+  end
+
+
 end
