83eca0f143773fdb16670eda41a2fd12361c78b9
Jonathan Pope <jonathanmpope@gmail.com>
2022-06-07 10:38:01 -0600

    test: food truck potential revenue test in event spec

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -52,5 +52,13 @@ RSpec.describe do
     expect(@event.food_trucks_that_sell(@item1)).to eq([@food_truck1, @food_truck3])
     expect(@event.food_trucks_that_sell(@item4)).to eq([@food_truck2])
   end
-    # require 'pry' ; binding.pry
+
+  it 'can list trucks that sell a specific item' do
+    @event.add_food_truck(@food_truck1)
+    @event.add_food_truck(@food_truck2)
+    @event.add_food_truck(@food_truck3)
+    expect(@food_truck1.potential_revenue).to eq(148.75)
+    expect(@food_truck2.potential_revenue).to eq(345.00)
+    expect(@food_truck3.potential_revenue).to eq(243.75)
+  end
 end
