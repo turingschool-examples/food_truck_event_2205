f9ebcf542e5e28712fc9642bd8d1591f8e01bb6c
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-06-07 10:51:41 -0500

    feat: add Event food_trucks_that_sell method

--- lib/event.rb
+++ lib/event.rb
@@ -12,4 +12,10 @@ class Event
     @food_truck_names << food_truck.name
     @food_trucks
   end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.select do |food_truck|
+      food_truck.inventory.keys.include?(item)
+    end
+  end
 end
